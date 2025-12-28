146e3e6a51e5e243c42c6cb54927d3156e87d176
Sharvil Lahute <sharvillahute1991@gmail.com>
2025-12-28 20:50:23 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -17,6 +17,12 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
+//routes
+import userRouter from './routes/user.routes.js'
 
+//routes declarations
+app.use("/api/v1/users", userRouter)
+
+//https://localhost:8000/api/v1/users/register
 
 export { app }  
